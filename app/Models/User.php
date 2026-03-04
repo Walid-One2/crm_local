deef67d570373b3a60cb47113913506f096639d2
Walid-One2 <elhamdaoui.walid123@gmail.com>
2026-03-04 13:31:03 +0000

    creation du model de user

---
+++ app/Models/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable, SoftDeletes;
+
+    protected $fillable = [
+        'fonction_id',
+        'responsable_id',
+        'statut',
+        'email',
+        'tel',
+        'password',
+        'roles',
+        'name',
+        'email_verified_at',
+        'remember_token',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function fonction()
+    {
+        return $this->belongsTo(Fonction::class);
+    }
+
+    public function responsable()
+    {
+        return $this->belongsTo(Responsable::class);
+    }
+}
