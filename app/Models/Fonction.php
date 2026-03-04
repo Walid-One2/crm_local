7f5f53f64399c796ffb7da091508979f6248dd3e
Walid-One2 <elhamdaoui.walid123@gmail.com>
2026-03-04 13:35:04 +0000

    creation du model de fonction

---
+++ app/Models/Fonction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Fonction extends Model
+{
+    protected $fillable = ['intitule'];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
