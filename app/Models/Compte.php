c608713da0f2623f3a80d86244398f3ea3cf352f
Walid-One2 <elhamdaoui.walid123@gmail.com>
2026-03-05 12:14:42 +0000

    creation du model Compte

---
+++ app/Models/Compte.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Compte extends Model
+{
+    protected $fillable = [
+        'id',
+        'pays_id',
+        'user_id',
+        'nom',
+        'description',
+        'adresse',
+        'complementadresse',
+        'cp',
+        'ville',
+        'siteweb',
+        'tel',
+        'tel2',
+        'fax',
+        'statut',
+        'date_creation',
+        'niveau_satisfaction',
+        'date_modification',
+        'email',
+        'statut_f',
+        'user_id_F',
+        'site_id',
+        'numero_ice',
+        'uid',
+        'categorie',
+        'date_note_devis',
+    ];
+
+    public function pays()
+    {
+        return $this->belongsTo(pays::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
